d6e1c8500834af2896bc1960b53ea58aec270d71
Carson Crow <carson.crow@texasbar.com>
2024-10-03 12:09:01 -0500

    .

--- src/config/tiers.config.ts
+++ src/config/tiers.config.ts
@@ -8,6 +8,14 @@ export const TIER_COSTS = {
 export const TIER_BONUSES = {
     weapon: {
         stat: "weapon_damage",
-        bonuses: [0.06, 0.1, 0.1, 0.18]
+        bonuses: [0.06, 0.1, 0.14, 0.18]
+    },
+    vitality: {
+      stat: "base_health",
+      bonuses: [0.11, 0.14, 0.17, 0.2]
+    },
+    spirit: {
+      stat: "spirit_power",
+      bonuses: [4, 8, 12, 16]
     }
 }
