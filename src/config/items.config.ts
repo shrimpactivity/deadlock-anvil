e87c0ee06ed2be1a628bceed614311959359d024
Cassidy Schulze <cassisurf@gmail.com>
2024-10-07 19:48:33 -0500

    added conditions and stats

--- src/config/items.config.ts
+++ src/config/items.config.ts
@@ -15,7 +15,7 @@ export const ITEMS: Item[] = [
     category: "weapon",
     tier: 1,
     stats: [{ name: "bullet_shield_health", amount: 85, type: "flat" }],
-    effect: {
+    passive: {
       condition: "close_range_15m",
       stats: [{ name: "weapon_damage", amount: 0.25, type: "percentage" }],
     },
@@ -28,7 +28,7 @@ export const ITEMS: Item[] = [
       { name: "fire_rate", amount: 0.04, type: "percentage" },
       { name: "bullet_shield_health", amount: 40, type: "flat" },
     ],
-    effect: {
+    passive: {
       condition: "headshot",
       stats: [{ name: "bonus_weapon_damage", amount: 40, type: "flat" }],
       cooldown: 7.5,
@@ -52,7 +52,7 @@ export const ITEMS: Item[] = [
       { name: "spirit_shield_health", amount: 95, type: "flat" },
       { name: "spirit_power", amount: 4, type: "flat" },
     ],
-    effect: {
+    passive: {
       condition: "health_above_60%",
       stats: [{ name: "bonus_weapon_damage", amount: 0.22, type: "percentage" }],
     },
@@ -85,12 +85,12 @@ export const ITEMS: Item[] = [
       { name: "bullet_resist", amount: .07, type: "percentage" },
       { name: "weapon_damage", amount: .08, type: "percentage" },
     ],
-    effect: {
+    passive: {
       condition: "hit_hero",
       stats: [{ name: "healing", amount: 40, type: "flat" }],
       cooldown: 5.5
     },
-    //effect: { TODO: //this one's weird because it has an either/or effect. If you hit a hero, you heal 40, if you hit a NPC you heal 15. Should this be 2 properties, or is there a way to add an If/Then?
+    //passive: { TODO: //this one's weird because it has an either/or passive. If you hit a hero, you heal 40, if you hit a NPC you heal 15. Should this be 2 properties, or is there a way to add an If/Then?
     //  condition: "hit_npc",
     //  stats: [{name: "healing", amount: 15, type: "flat"}]
     //  cooldown: 5.5
@@ -104,7 +104,7 @@ export const ITEMS: Item[] = [
       { name: "reload_time", amount: -0.2, type: "percentage" },// TODO: making sure it's clear that this is a negative percentage
       { name: "weapon_damage", amount: 0.1, type: "percentage" },
     ],
-    effect: {
+    passive: {
       condition: "active_reload",
       stats: [{ name: "fire_rate", amount: 0.2, type: "percentage" },
         {name: "bullet_lifesteal", amount: 0.2, type: "percentage"}
@@ -130,7 +130,7 @@ export const ITEMS: Item[] = [
       { name: "bonus_health", amount: 100, type: "flat" },
       { name: "health_regen", amount: 1.5, type: "flat" },
     ],
-    effect: {
+    passive: {
       condition: "kinetic_dash",
       stats: [{name: "dash_jump", amount: 0, type: "flat"}, //TODO: it makes your next dash jump free, so I'm wondering if it makes sense for this modifier to be for stamina? I'm not sure how we want to remove the cost of something
         {name: "fire_rate", amount: 0.2, type: "percentage"},
@@ -148,7 +148,7 @@ export const ITEMS: Item[] = [
       { name: "ammo", amount: 0.2, type: "percentage" },
       { name: "bullet_shield_health", amount: 140, type: "flat" },
     ],
-    effect: {
+    passive: {
       condition: "long_range_15m",
       stats: [{name: "weapon_damage", amount: 0.4, type: "percentage"}]
     }
@@ -163,7 +163,7 @@ export const ITEMS: Item[] = [
       { name: "health_regen", amount: 1, type: "flat"},
       { name: "heavy_melee_distance", amount: 0.4, type: "percentage"}
     ],
-    effect: { //TODO: there's an additional either/or effect. You either instantly reload or get up to 100% bonus ammo
+    passive: { //TODO: there's an additional either/or passive. You either instantly reload or get up to 100% bonus ammo
       condition: "heavy_melee",
       stats: [{ name: "bonus_heavy_melee_damage", amount: 0.2, type: "percentage"}],
       cooldown: 10 
