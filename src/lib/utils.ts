faa9dd7e29e59a0c26b1ef7fd3246ef8f212b554
Carson Crow <carson.crow@texasbar.com>
2024-10-09 14:36:32 -0500

    added util tests

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,5 @@
-export function underscoreToCapitalCase(str: string): string {
+export function underscoreToUpperCase(str: string): string {
   const words = str.split("_");
-  const capitalWords = words.map(word => word.charAt(0))
-  
+  const capitalWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+  return capitalWords.join(' ');
 }
